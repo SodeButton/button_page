7378ce1bb00934179f0c8b4d656066ae2e15321f
Button <sodebotannisei.2002@gmail.com>
2021-01-30 16:59:02 +0900

    update

--- apps/stg/main.js
+++ apps/stg/main.js
@@ -1,30 +1,53 @@
 /**
- * Author Button501
- * License MIT
+ * Author : Button501
+ * License : MIT License
  */
 
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    fps: {
-        target: 60,
-        forceSetTimeOut: true
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+	type: Phaser.AUTO,
+	parent: 'canvas',
+	width: 800,
+	height: 600,
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+	}, 
+	render: {
+		pixelArt: true
+	},
+	fps: {
+		target: 60,
+		forceSetTimeOut: true
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 0 }
+		}
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
 }
 
 const game = new Phaser.Game(config);
+
+class Player extends Phaser.Physics.Arcade.Sprite {
+	constructor(scene, x, y, texture, frame) {
+	  super(scene, x, y, texture, frame);
+	  this.scene = scene;
+	  //シーンのDisplayListとUpdateListにゲームを追加する
+	  this.scene.add.existing(this);
+	  //Arcade物理ボディをゲームオブジェクトに追加する
+	  this.scene.physics.world.enable(this);
+	  this.setTexture(texture, frame);
+	  this.setPosition(x, y);
+	  this.setSize(1, 1);
+	  this.setOffset(0, 0);
+	}
+}
 
 let player;
 let speed = 4;
@@ -32,66 +55,56 @@ let offset = new Object();
 offset.x = 0;
 offset.y = 0;
 
-let enemyList = new Array();
+let enemyList;
 let enemySpawnTime = 0;
 
 function preload() {
-    //this.load.setBaseURL('https://sodebutton.github.io');
-    this.load.image("player", "./Resources/Sprites/player.png");
-    this.load.image("enemy", "./Resources/Sprites/enemy.png");
+	//this.load.setBaseURL('https://sodebutton.github.io');
+	this.load.image("player", "./Resources/Sprites/player.png");
+	this.load.image("enemy", "./Resources/Sprites/enemy.png");
 }
 
 function create() {
-    //player = this.add.image(400, 300, 'player');
+	//player = this.add.image(400, 300, 'player');
 
-    player = this.physics.add.image(400, 100, 'player');
-
-    //player.setVelocity(100, 200);
-    //player.setBounce(1, 1);
-    player.setCollideWorldBounds(true);
+	player = new Player(this, 300, 400, 'player', 0);
+	enemyList = this.add.group();
+	//player.setVelocity(100, 200);
+	//player.setBounce(1, 1);
+	//player.setCollideWorldBounds(true);
 }
 
 function update() {
-    var cursors = this.input.keyboard.createCursorKeys();
+	var cursors = this.input.keyboard.createCursorKeys();
 
-    if(cursors.left.isDown) {
-        player.x -= speed;
-        //player.anims.play('left', true);
-    }
-    if(cursors.right.isDown) {
-        player.x += speed;
-    }
-    if(cursors.up.isDown) {
-        player.y -= speed;
-    }
-    if(cursors.down.isDown) {
-        player.y += speed;
-    }
+	this.input.on('pointerdown', function(pointer) {
+		offset.x = player.x -pointer.x;
+		offset.y = player.y - pointer.y;
+	});
 
-    this.input.on('pointerdown', function(pointer) {
-        offset.x = player.x -pointer.x;
-        offset.y = player.y - pointer.y;
-    });
+	var pointer = this.input.activePointer;
+	if (pointer.isDown) {
+		player.x = pointer.x + offset.x;
+		player.y = pointer.y + offset.y;
 
-    var pointer = this.input.activePointer;
-    if (pointer.isDown) {
-        player.x = pointer.x + offset.x;
-        player.y = pointer.y + offset.y;
-        console.log(player.x);
-        console.log(player.y);
-    }
+		if(player.x < player.width / 2) player.x = player.width / 2;
+		if(player.x > this.scale.width - (player.width / 2)) player.x = this.scale.width - (player.width / 2);
+		if(player.y < player.height / 2) player.y = player.height / 2;
+		if(player.y > this.scale.height - (player.height / 2)) player.y = this.scale.height - (player.height / 2);
+		
+	}
 
-    enemySpawnTime--;
-    if(enemySpawnTime <= 0) {
-        enemyList[enemyList.length] = this.add.image(Math.random() * (800-32) + 16, 0, 'enemy');
-        enemySpawnTime += 120;
-    }
+	enemySpawnTime--;
+	if(enemySpawnTime <= 0) {
+		enemyList.add(this.add.image(Math.random() * (800-32) + 16, 0, 'enemy'));
+		enemySpawnTime += 120;
+	}
 
-    enemyList.forEach(enemy => {
-        enemy.y += 2;
+	enemyList.getChildren().forEach(enemy => {
+		enemy.y += 2;
 
-        if(enemy.y > 616) {
+		if(enemy.y > 616) {
 
-        }
-    });
+		}
+	});
 }
